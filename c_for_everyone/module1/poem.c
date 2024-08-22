efda188d9d4811ded5e3c2712959ceafaf36c40d
sarahbuyiwater <sarah@buyiwater.com>
2024-08-21 20:17:50 -0700

    poem assignment..

---
+++ c_for_everyone/module1/poem.c
@@ -0,0 +1,12 @@
+/*
+A poem I wrote with newlines and also it rhymes
+Aug 21, 2024
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("\nOf this course, I couldn't be fonder!\nWhat could go wrong!\nBut now I'm left to ponder...\nIs this print statement too long?\n\n");
+    return 0;
+}
