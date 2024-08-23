c22fbd9d7dafb1485beed372e532348be73458bc
searspeters <sarahbentzley@gmail.com>
2024-08-22 20:11:30 -0700

    assignment updated

--- c_for_everyone/module2/assignment.c
+++ c_for_everyone/module2/assignment.c
@@ -13,5 +13,8 @@ int main(void)
     printf("a = %d, b = %d, c = %d\n", a, b, c);
     average = ( a + b + c) / 3.0; // conversion if we do 3
     printf("average = %lf\n", average);
+
+    
+
     return 0;
 }
